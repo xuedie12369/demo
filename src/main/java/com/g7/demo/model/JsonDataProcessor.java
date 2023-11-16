a4f8a498d9b7392bce17925e248f753acea9907e
MyProfile <501968797@qq.com>
2023-11-16 18:30:29 +0800

    优化代码

--- src/main/java/com/g7/demo/model/JsonDataProcessor.java
+++ src/main/java/com/g7/demo/model/JsonDataProcessor.java
@@ -1,6 +1,7 @@
 package com.g7.demo.model;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -18,8 +19,8 @@ public class JsonDataProcessor {
     public static Map getConfigMap(String name, Map<String, HashMap<String, String>> configs) {
         //HashMap<String, HashMap<String, String>> configs = getConfigs();
         HashMap<String, String> coldchain_biz_server = configs.get(name);
-        if (CollUtil.isEmpty(coldchain_biz_server)){
-            System.out.println("查询数据为空"+name);
+        if (CollUtil.isEmpty(coldchain_biz_server)) {
+            System.out.println("查询数据为空" + name);
             return null;
         }
         //writer.append("AppId,Redis,MySQL,Mongo,HBase,DRDS,RocketMQ,Kafka,OSS,ZooKeeper\n");
@@ -27,39 +28,119 @@ public class JsonDataProcessor {
 //| redis | mysql | mongo | hbase
 // 未处理的  // | drds| RocketMQ | kafka | 域名 |
 // OSS信息 | zk信息|等
-        Map result = new HashMap<String, String>();
-        Map redisMap = getRedisMap(coldchain_biz_server);
-        result.putAll(redisMap);
-        //mysql的
-        Map<String, String> mysql = filterMapByRegex(coldchain_biz_server, "spring\\.datasource\\..*\\.read\\.jdbcUrl");
-        Map<String, String> oss = filterMapByRegex(coldchain_biz_server, "oss\\..*");
-        Map<String, String> MongoDB = filterMapByRegex(coldchain_biz_server, "spring\\.data\\.mongodb\\..*");
-        Map<String, String> zookeeper = filterMapByRegex(coldchain_biz_server, "zookeeper\\..*");
-        Map<String, String> hbase = filterMapByRegex(coldchain_biz_server, "hbase\\..*");
-        Map<String, String> kafka = filterMapByRegex(coldchain_biz_server, "kafka.consumer.servers\\..*");
-        Map<String, String> kafka2 = filterMapByRegex(coldchain_biz_server, "kafka.brokers\\..*");
-        Map<String, String> rocketmq = filterMapByRegex(coldchain_biz_server, "rocketmq.consumer\\..*");
-        //Map<String, String> hbase = filterMapByRegex(coldchain_biz_server, "hbase.\\..*");
-        result.putAll(rocketmq);
-        result.putAll(mysql);
-        //result.putAll(mysql);
-        result.putAll(kafka);
-        result.putAll(kafka2);
-        result.putAll(oss);
-        result.putAll(MongoDB);
-        result.putAll(zookeeper);
-        result.putAll(hbase);
+        Map result = fileterMap(name, coldchain_biz_server);
         return result;
 
     }
 
-    private static Map<String, String> filterMapByRegex(Map<String, String> originalMap, String regex) {
+    static Map fileterMap(String name, HashMap<String, String> coldchain_biz_server) {
+        Map result = new HashMap<String, String>();
+        Map redisMap = getRedisMap(coldchain_biz_server);
+        Map<String, String> redis = filterMapByRegex(coldchain_biz_server,
+                ".*\\.redis\\..*"
+                ,"redis\\..*");
+        //Map<String, String> redis3 = filterMapByRegex(coldchain_biz_server, "spring\\.redis\\..*");
+        result.putAll(redis);
+        //result.putAll(redis3);
+        result.putAll(redisMap);
+        //mysql的
+        Map<String, String> mysql = filterMapByRegex(coldchain_biz_server, "spring\\.datasource\\..*\\.read\\.jdbcUrl");
+        Map<String, String> rds = filterMapByRegex(coldchain_biz_server, "spring\\.datasource\\..*\\.rds\\..*");
+        Map<String, String> oss = filterMapByRegex(coldchain_biz_server,
+                "oss\\..*"
+                ,".*oss.*"
+                ,".*Oss.*"
+        );
+        Map<String, String> MongoDB = filterMapByRegex(coldchain_biz_server, "spring\\.data\\.mongodb\\.*");
+        Map<String, String> zookeeper = filterMapByRegex(
+                coldchain_biz_server,
+                "zookeeper\\..*",
+                "zookeeper.address.host",
+                "primary.hbase.zookeeper.quorum",
+                ".*\\.zookeeper\\..*"
+                );
+        Map<String, String> hbase = filterMapByRegex(coldchain_biz_server, "hbase\\..*");
+        Map<String, String> kafka = filterMapByRegex(coldchain_biz_server, "kafka\\.consumer\\.servers\\.*");
+        Map<String, String> kafka2 = filterMapByRegex(
+                coldchain_biz_server, "kafka\\.broker\\..*");
+        Map<String, String> kafka3 = filterMapByRegex(coldchain_biz_server,
+                "kafka\\.producer\\.topic\\.*",
+                "kafka\\.\\w+\\.consumer\\.group\\.id",
+                "kafka\\.producer\\.bootstrap-servers");
+        Map<String, String> rocketmq = filterMapByRegex(coldchain_biz_server, "rocketmq\\.consumer\\..*");
+        Map<String, String> db = filterMapByRegex(coldchain_biz_server, "spring\\.datasource\\..*\\.url");
+        Map<String, String> mq = filterMapByRegex(coldchain_biz_server, "mq.nameserver");
+        Map<String, String> mysqlUrl = filterMapByRegex(coldchain_biz_server, "mysql.url");
+        Map<String, String> kafkaf = filterMapByRegex(coldchain_biz_server, "kafka.loadEvent.topic");
+        Map<String, String> brokers = filterMapByRegex(coldchain_biz_server,
+                "kafka.brokers",
+                "kafka.servers",
+                "kafka.producer.smooth.topic",
+                "kafka.consumer.\\..*\\.topic"
+                ,"bucketName"
+                ,"kafka.kdc"
+        );
+        Map<String, String> group = filterMapByRegex(coldchain_biz_server, "kafka.loadEvent.group.id");
+        result.putAll(mysqlUrl);
+        result.putAll(kafkaf);
+        result.putAll(brokers);
+        result.putAll(group);
+
+        //Map<String, String> hbase = filterMapByRegex(coldchain_biz_server, "hbase.\\..*");
+        if ("load-ird-partner-algorithm".equals(name)) {
+            System.out.println("asdasd");
+        }
+        result.putAll(db);
+        result.putAll(mq);
+        result.putAll(rocketmq);
+        result.putAll(mysql);
+        result.putAll(rds);
+        //result.putAll(mysql);
+        result.putAll(kafka);
+        result.putAll(kafka2);
+        result.putAll(kafka3);
+        result.putAll(oss);
+        result.putAll(MongoDB);
+        result.putAll(zookeeper);
+        result.putAll(hbase);
+
+
+        result.remove("hbase.client.username");
+        result.remove("hbase.client.username");
+        result.remove("spring.redis.password");
+        result.remove("spring.redis.area.lettuce.pool.min-idle");
+        result.remove("spring.redis.area.lettuce.pool.max-wait");
+        filterMap(result, "spring\\.datasource\\..*\\.driverClassName");
+        filterMap(result, "spring\\.datasource\\..*\\.password");
+        filterMap(result, "spring\\.datasource\\..*\\.username");
+        filterMap(result, "spring\\.redis\\..*\\.username");
+        filterMap(result, "spring\\.redis\\..*\\.password");
+        filterMap(result, "spring\\.redis\\..*\\.time");
+        filterMap(result, "aspect.distribution.redis.lock.enable");
+        filterMap(result, "spring\\.redis\\..*\\.shutdown-timeout");
+        filterMap(result, "spring\\.redis\\..*\\.max-wait");
+        return result;
+    }
+
+    private static void filterMap(Map<String, String> map, String regex) {
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = iterator.next();
+            if (  Pattern.compile(regex).matcher(entry.getKey()).matches()) {
+                iterator.remove(); // 删除符合条件的键值对
+            }
+        }
+    }
+    private static Map<String, String> filterMapByRegex(Map<String, String> originalMap, String ...regexs) {
         Map<String, String> filteredMap = new LinkedHashMap<>();
-        Pattern pattern = Pattern.compile(regex);
-        for (Map.Entry<String, String> entry : originalMap.entrySet()) {
-            String key = entry.getKey();
-            if (pattern.matcher(key).matches()) {
-                filteredMap.put(key, entry.getValue());
+        for (String regex : regexs) {
+
+            Pattern pattern = Pattern.compile(regex);
+            for (Map.Entry<String, String> entry : originalMap.entrySet()) {
+                String key = entry.getKey();
+                if (pattern.matcher(key).matches()) {
+                    filteredMap.put(key, entry.getValue());
+                }
             }
         }
         return filteredMap;
@@ -70,10 +151,15 @@ public class JsonDataProcessor {
         String database = "spring.redis.database";
         String host = "spring.redis.host";
         String port = "spring.redis.port";
-
-        objectObjectHashMap.put(database, stringStringMap.get(database));
-        objectObjectHashMap.put(host, stringStringMap.get(host));
-        objectObjectHashMap.put(port, stringStringMap.get(port));
+        if (StrUtil.isNotBlank(stringStringMap.get(database))) {
+            objectObjectHashMap.put(database, stringStringMap.get(database));
+        }
+        if (StrUtil.isNotBlank(stringStringMap.get(host))) {
+            objectObjectHashMap.put(host, stringStringMap.get(host));
+        }
+        if (StrUtil.isNotBlank(stringStringMap.get(port))) {
+            objectObjectHashMap.put(port, stringStringMap.get(port));
+        }
 
         return objectObjectHashMap;
     }
@@ -111,7 +197,7 @@ public class JsonDataProcessor {
         return null;
     }
 
-    private static Map<String, HashMap<String, String>> getConfigs(Map<String, Map<String, JsonNode>> groupedData) {
+    static Map<String, HashMap<String, String>> getConfigs(Map<String, Map<String, JsonNode>> groupedData) {
         Map<String, HashMap<String, String>> result = new TreeMap<>();
         groupedData.forEach((k, v) -> {
             HashMap<String, String> objectObjectHashMap = new HashMap<>();
@@ -140,7 +226,7 @@ public class JsonDataProcessor {
         }
     }
 
-    private static Map<String, Map<String, JsonNode>> parseJson(String json) throws IOException {
+    public static Map<String, Map<String, JsonNode>> parseJson(String json) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         List<List<String>> rows = objectMapper.readValue(json, List.class);
 
--- src/main/java/com/g7/demo/model/JsonToExcel.java
+++ src/main/java/com/g7/demo/model/JsonToExcel.java
@@ -8,6 +8,7 @@ import cn.hutool.core.collection.CollUtil;
  * @description
  */
 import cn.hutool.json.JSONUtil;
+import cn.hutool.poi.excel.ExcelUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.poi.ss.usermodel.*;
@@ -20,24 +21,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonToExcel {
 
     public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         ArrayList<String> files = new ArrayList<>();
-        String baseDir="/Users/mac/g7e6/coldchain_biz_server/src/main/java/com/chinaway/intelli_trailer/model/";
-        String 量方平台产品 = baseDir+"量方平台产品"+".json";
-        String 冷链平台产品 = baseDir+"冷链平台产品"+".json";
-        String 载重平台产品 = baseDir+"载重平台产品"+".json";
-        files.add(量方平台产品);
-        files.add(冷链平台产品);
-        files.add(载重平台产品);
+        String baseDir = "/Users/mac/g7e6/demo/src/main/resources/";
+        String 量方平台产品 = baseDir + "量方平台产品" + ".json";
+        String 冷链平台产品 = baseDir + "冷链平台产品" + ".json";
+        String 载重平台产品 = baseDir + "载重平台产品" + ".json";
+        String 车次中心 = baseDir + "车次中心" + ".json";
+        String 追货 = baseDir + "追货" + ".json";
+        //files.add(量方平台产品);
+        //files.add(冷链平台产品);
+        //files.add(车次中心);
+        //files.add(载重平台产品);
+        files.add(追货);
         // 创建Excel工作簿
         //Workbook workbook = new XSSFWorkbook();
         //Sheet sheet = workbook.createSheet("Applications");
         //Map<String, HashMap<String, String>> configs = JsonDataProcessor.getConfigs();
-
 
 
         try {
@@ -45,6 +51,7 @@ public class JsonToExcel {
                 Workbook workbook = new XSSFWorkbook();
                 Sheet sheet = workbook.createSheet("Applications");
                 Map<String, HashMap<String, String>> configs = JsonDataProcessor.getConfigs();
+                //Map<String, HashMap<String, String>> configs = JsonDataProcessor.getConfigs(JsonDataProcessor.parseJson("{\"spring.datasource.write.url\":\"jdbc:mysql://rm-bp1974ya0y45q9671.mysql.rds.aliyuncs.com:3306/intelli_trailer?useUnicode\\u003dtrue\\u0026characterEncoding\\u003dutf8\\u0026useSSL\\u003dfalse\\u0026allowMultiQueries\\u003dtrue\\u0026zeroDateTimeBehavior\\u003dconvertToNull\\u0026tinyInt1isBit\\u003dfalse\",\"spring.datasource.gto.password\":\"MHz1YJGmymhUG0Y0\",\"spring.redis.database\":\"2\",\"spring.redis.password\":\"KWboR9tNTS8nqG4Y\",\"spring.datasource.read.password\":\"guache_rw@20!(\",\"spring.datasource.read.url\":\"jdbc:mysql://rm-bp1974ya0y45q9671.mysql.rds.aliyuncs.com:3306/intelli_trailer?useUnicode\\u003dtrue\\u0026characterEncoding\\u003dutf8\\u0026useSSL\\u003dfalse\\u0026allowMultiQueries\\u003dtrue\\u0026zeroDateTimeBehavior\\u003dconvertToNull\\u0026tinyInt1isBit\\u003dfalse\",\"spring.redis.host\":\"r-bp1d92b5c065bfa4.redis.rds.aliyuncs.com\",\"spring.datasource.gto.username\":\"g7trailer_rw\",\"spring.datasource.amc.username\":\"g7amc_rw\",\"spring.datasource.amc.url\":\"jdbc:mysql://rm-bp1974ya0y45q9671.mysql.rds.aliyuncs.com:3306/g7amc?useUnicode\\u003dtrue\\u0026characterEncoding\\u003dutf8\\u0026useSSL\\u003dfalse\\u0026allowMultiQueries\\u003dtrue\\u0026zeroDateTimeBehavior\\u003dconvertToNull\\u0026tinyInt1isBit\\u003dfalse\",\"spring.redis.port\":\"6379\",\"spring.datasource.write.username\":\"g7amc_rw\",\"spring.datasource.amc.password\":\"YlME7vXTiao9lmS2\",\"spring.datasource.read.username\":\"guache_rw\",\"spring.datasource.gto.url\":\"jdbc:mysql://rm-bp1974ya0y45q9671.mysql.rds.aliyuncs.com:3306/g7trailer?useUnicode\\u003dtrue\\u0026characterEncoding\\u003dutf8\\u0026useSSL\\u003dfalse\\u0026allowMultiQueries\\u003dtrue\\u0026zeroDateTimeBehavior\\u003dconvertToNull\\u0026tinyInt1isBit\\u003dfalse\",\"spring.datasource.load.password\":\"B3JVaGFdNw90SNZp\",\"spring.datasource.write.password\":\"YlME7vXTiao9lmS2\",\"spring.datasource.load.url\":\"jdbc:mysql://rm-bp1spwv1148p7wx06.mysql.rds.aliyuncs.com:3306/basic_bearing?useUnicode\\u003dtrue\\u0026characterEncoding\\u003dutf8\\u0026useSSL\\u003dfalse\\u0026allowMultiQueries\\u003dtrue\\u0026zeroDateTimeBehavior\\u003dconvertToNull\\u0026tinyInt1isBit\\u003dfalse\",\"spring.datasource.load.username\":\"intellitrailer\"}\n"););
 
                 String baseServiceURl = "https://devops.chinawayltd.com/system/deliver/application/detail?id=";
                 // 从外部文件读取JSON数据
@@ -58,6 +65,7 @@ public class JsonToExcel {
                 headerRow.createCell(1).setCellValue("devops地址");
                 headerRow.createCell(2).setCellValue("git地址");
                 headerRow.createCell(3).setCellValue("资源依赖项");
+                headerRow.createCell(4).setCellValue("是否自动生成");
 
                 // 填充数据
                 int rowNum = 1;
@@ -72,19 +80,37 @@ public class JsonToExcel {
                     row.createCell(1).setCellValue(baseServiceURl + application.get("ID").asText());
                     //row.createCell(1).setCellValue(application.get("name").asText());
                     //git地址
-                    row.createCell(2).setCellValue(application.get("code_repositories").asText());
-                    Map configMap = JsonDataProcessor.getConfigMap(name,configs);
-                    if (CollUtil.isNotEmpty(configMap)){
+                    String code_repositories = application.get("code_repositories").asText();
+
+                    row.createCell(2).setCellValue((code_repositories));
+                    Map configMap = JsonDataProcessor.getConfigMap(name, configs);
+                    if (CollUtil.isNotEmpty(configMap)) {
                         row.createCell(3).setCellValue(JSONUtil.formatJsonStr(configMap.toString()));
-                    }else{
-                        Map configMap2= JsonDataProcessor.getConfigMap(  replaceUnderscore(name),configs);
-                        if (CollUtil.isNotEmpty(configMap2)){
+                        row.createCell(4).setCellValue("自动生成数据");
+
+                    } else {
+                        Map configMap2 = JsonDataProcessor.getConfigMap(replaceUnderscore(name), configs);
+                        if (CollUtil.isNotEmpty(configMap2)) {
                             row.createCell(3).setCellValue(JSONUtil.formatJsonStr(configMap2.toString()));
+                            row.createCell(4).setCellValue("自动生成数据");
+                        }else{
+                            name = extractRepoName(code_repositories);
+                            Map configMap3 = JsonDataProcessor.getConfigMap(replaceUnderscore(name), configs);
+                            if (CollUtil.isNotEmpty(configMap3)) {
+                                row.createCell(3).setCellValue(JSONUtil.formatJsonStr(configMap3.toString()));
+                                row.createCell(4).setCellValue("自动生成数据");
+                            }else{
+                                Map configMap4 = JsonDataProcessor.getConfigMap((name), configs);
+                                if (CollUtil.isNotEmpty(configMap4)) {
+                                    row.createCell(3).setCellValue(JSONUtil.formatJsonStr(configMap4.toString()));
+                                    row.createCell(4).setCellValue("自动生成数据");
+                                }
+                            }
                         }
                     }
                 }
                 Row row = sheet.createRow(rowNum++);
-                saveFile(workbook, getFileNameWithoutExtension(jsonFile)+".xlsx");
+                saveFile(workbook, getFileNameWithoutExtension(jsonFile) + ".xlsx");
             }
             // 自动打开文件所在目录
             // 生成保存文件的路径
@@ -94,10 +120,12 @@ public class JsonToExcel {
         //// 生成保存文件的路径
         //saveFile(workbook);
     }
+
     private static String replaceUnderscore(String input) {
         // 使用 replace 方法进行替换
-        return input.replace( '-','_');
+        return input.replace('-', '_');
     }
+
     private static void saveFile(Workbook workbook, String fileName) throws IOException {
         //String outputPath = "applications.xlsx";
 
@@ -111,9 +139,8 @@ public class JsonToExcel {
 
         // 打印保存的文件路径
         File file1 = new File(fileName);
-
-        System.out.println("输出文件: " );
-        System.out.println(file1.getAbsolutePath() );
+        System.out.println("输出文件: ");
+        System.out.println(file1.getAbsolutePath());
     }
 
 
@@ -134,5 +161,14 @@ public class JsonToExcel {
             return fileName;
         }
     }
+
+    public static String extractRepoName(String gitUrl) {
+        // 使用最简单的方法，通过split和replace
+        String[] parts = gitUrl.split("/");
+        String lastPart = parts[parts.length - 1];
+        String repoName = lastPart.replace(".git", "");
+        return repoName;
+    }
+
 }
 
